4c31e7a616778800d5505e6d8572ff3b2282a39b
Jonesh Shrestha <contact@joneshshrestha.com>
2019-05-24 17:02:49 +0545

    commit added

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
-let values = []
-
+let value = []
 let i = 0
 let w = 10
 
 function setup() {
-    createCanvas(800, 200);
-    values = new Array(floor(width / w));
-    for (let i = 0; i < values.length; i++) {
-        values[i] = random(height);
+    createCanvas(800, 200);                           // CreateCanvas(width, height) 
+    values = new Array(floor(width / w));             // Creates an array of length 80 but without any elements in the array
+    for (let i = 0; i < values.length; i++) {         // While i < 80 runs the loop
+        values[i] = random(height);                   // Fill the array with random heights (0 < height < 200 )
     }
     frameRate(5)
 }
@@ -15,15 +14,15 @@ function setup() {
 function draw() {
     background(51);
 
-    for (let i = 0; i < values.length; i++) {
-        stroke(0)
-        fill(255)
-        rect(i * w, height - values[i], w, values[i])
+    for (let i = 0; i < values.length; i++) {         
+        stroke(0)                                     // Black border of rectangle
+        fill(255)                                     // White fill inside the rectangle
+        rect(i * w, height - values[i], w, values[i]) // rect(x, y, width, height)
     }
 }
 
 function swap(arr, a, b) {
-    let temp = arr[a]
+    let temp = arr[a]                                 
     arr[a] = arr[b]
     arr[b] = temp
 }
