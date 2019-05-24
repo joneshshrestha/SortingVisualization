d031e3784e1258acd87c0a447eb4d63a5c189f71
Jonesh Shrestha <contact@joneshshrestha.com>
2019-05-24 16:14:10 +0545

    random bar generation

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let values = []
+
+let i = 0
+let w = 10
+
+function setup() {
+    createCanvas(800, 200);
+    values = new Array(floor(width / w));
+    for (let i = 0; i < values.length; i++) {
+        values[i] = random(height);
+    }
+    frameRate(5)
+}
+
+function draw() {
+    background(51);
+
+    for (let i = 0; i < values.length; i++) {
+        stroke(0)
+        fill(255)
+        rect(i * w, height - values[i], w, values[i])
+    }
+}
+
+function swap(arr, a, b) {
+    let temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+}
