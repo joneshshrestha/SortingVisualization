e7c6d08976840cea6e05f37c0dffe84aba2f5ec5
Jonesh Shrestha <contact@joneshshrestha.com>
2019-07-05 14:18:47 +0545

    update

--- sketch.js
+++ sketch.js
@@ -12,25 +12,27 @@ function setup() {
     quickSort(values, 0, values.length - 1)           // Passed array, start = 0, end = arrayLength-1
 }
 
-function quickSort(arr, start, end) {                 // Recursive quickSort algorithm
+async function quickSort(arr, start, end) {           // Recursive quickSort algorithm
     if (start >= end) {                               // If array has more than 1 element than proceed to function else end the function
         return
     }
-    let index = partition(arr, start, end)            // 
-    quickSort(arr, start, index - 1)                  // Performs quickSort from 0 to 
-    quickSort(arr, index + 1, end) 
+    let index = await partition(arr, start, end)
+    await Promise.all([                               // Perform both quickSort simultaneously
+        quickSort(arr, start, index - 1),
+        quickSort(arr, index + 1, end) 
+    ])                            
 }
 
-function partition(arr, start, end) {                 
+async function partition(arr, start, end) {                 
     let pivotIndex = start                            // pivotIndex = 0
     let pivotValue = arr[end]                         // pivotValue = value of last element in the array
     for (let i = start; i < end; i++) {               // Runs loop from 0 to end of the array
         if (arr[i] < pivotValue) {                    // If array value at position i < last array value then
-            swap(arr, i, pivotIndex)                  // Swap array value at i and array value at pivotIndex 
+            await swap(arr, i, pivotIndex)            // Swap array value at i and array value at pivotIndex 
             pivotIndex++                              
         }
     }
-    swap(arr, pivotIndex, end)                        // Swap latest array value at pivotIndex and end value of the array 
+    await swap(arr, pivotIndex, end)                  // Swap latest array value at pivotIndex and end value of the array 
     return pivotIndex                                 // Return the sorted value
 }
 
@@ -44,8 +46,13 @@ function draw() {
     }
 }
 
-function swap(arr, a, b) {                            // Swap function
+async function swap(arr, a, b) {                      // Swap function
+    await sleep(25)                                 // Wait 1000ms before swap
     let temp = arr[a]                                 
     arr[a] = arr[b]
     arr[b] = temp
+}
+
+function sleep(ms) {                                            // Wait function
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
